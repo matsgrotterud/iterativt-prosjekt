cbf3b2df889b03b767c83b1e2571a771c5b0d5e9
Mats Grøtterud <mats@grotterud.no>
2016-03-30 00:53:42 +0200

    tester noe mulig dashboard funksjonalitet

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('#navigation ul li a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('#navigation ul li a').each(function () {
+            $(this).parent().removeClass('active');
+        })
+        $(this).parent().addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $("div.page").removeClass("open");
+        $target.addClass("open");
+        $('html, body').animate({
+            scrollTop: $(".meny-wrap").offset().top - 52
+        }, 0);
+    });
+});
