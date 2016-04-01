85e882698e7a5fe370ec384320e06fb2408efe0b
Duy Huong Nguyen <Duy Huong Nguyen>
2016-04-01 15:27:43 +0200

    fikset bug

--- ITERATION_1/assets/php/get_nyheter.php
+++ ITERATION_1/assets/php/get_nyheter.php
@@ -15,8 +15,6 @@
     $result = mysqli_query($connection, $query);
         
     while ($nyhet = mysqli_fetch_array($result)) {
-        echo $i;
-        echo '<br>';
         
         if($i == "0"){
             $GLOBALS['1tittel'] = $nyhet['tittel'];
@@ -60,86 +58,86 @@
 
     //Nyhet 1
     function getNyhet1Tittel(){
-        echo $GLOBALS['1tittel']."<br>";
+        echo $GLOBALS['1tittel']."";
     }
 
     function getNyhet1Navn(){
-        echo $GLOBALS['1navn']."<br>";
+        echo $GLOBALS['1navn']."";
     }
 
     function getNyhet1Beskrivelse(){
-        echo $GLOBALS['1beskrivelse']."<br>";
+        echo $GLOBALS['1beskrivelse']."";
     }
     
     function getNyhet1Tidspunkt(){
-        echo $GLOBALS['1tidspunkt']."<br>";
+        echo $GLOBALS['1tidspunkt']."";
     }
 
     //Nyhet 2
     function getNyhet2Tittel(){
-        echo $GLOBALS['2tittel']."<br>";
+        echo $GLOBALS['2tittel']."";
     }
 
     function getNyhet2Navn(){
-        echo $GLOBALS['2navn']."<br>";
+        echo $GLOBALS['2navn']."";
     }
 
     function getNyhet2Beskrivelse(){
-        echo $GLOBALS['2beskrivelse']."<br>";
+        echo $GLOBALS['2beskrivelse']."";
     }
     
     function getNyhet2Tidspunkt(){
-        echo $GLOBALS['2tidspunkt']."<br>";
+        echo $GLOBALS['2tidspunkt']."";
     }
 
     //Nyhet 3
     function getNyhet3Tittel(){
-        echo $GLOBALS['3tittel']."<br>";
+        echo $GLOBALS['3tittel']."";
     }
 
     function getNyhet3Navn(){
-        echo $GLOBALS['3navn']."<br>";
+        echo $GLOBALS['3navn']."";
     }
 
     function getNyhet3Beskrivelse(){
-        echo $GLOBALS['3beskrivelse']."<br>";
+        echo $GLOBALS['3beskrivelse']."";
     }
     
     function getNyhet3Tidspunkt(){
-        echo $GLOBALS['3tidspunkt']."<br>";
+        echo $GLOBALS['3tidspunkt']."";
     }
 
     //Nyhet 4
     function getNyhet4Tittel(){
-        echo $GLOBALS['4tittel']."<br>";
+        echo $GLOBALS['4tittel']."";
     }
 
     function getNyhet4Navn(){
-        echo $GLOBALS['4navn']."<br>";
+        echo $GLOBALS['4navn']."";
     }
 
     function getNyhet4Beskrivelse(){
-        echo $GLOBALS['4beskrivelse']."<br>";
+        echo $GLOBALS['4beskrivelse']."";
     }
     
     function getNyhet4Tidspunkt(){
-        echo $GLOBALS['4tidspunkt']."<br>";
+        echo $GLOBALS['4tidspunkt']."";
     }
  
     //Nyhet 5
     function getNyhet5Tittel(){
-        echo $GLOBALS['5tittel']."<br>";
+        echo $GLOBALS['5tittel']."";
     }
 
     function getNyhet5Navn(){
-        echo $GLOBALS['5navn']."<br>";
+        echo $GLOBALS['5navn']."";
     }
 
     function getNyhet5Beskrivelse(){
-        echo $GLOBALS['5beskrivelse']."<br>";
+        echo $GLOBALS['5beskrivelse']."";
     }
     
     function getNyhet5Tidspunkt(){
-        echo $GLOBALS['5tidspunkt']."<br>";
+        echo $GLOBALS['5tidspunkt']."";
     }
 ?>
